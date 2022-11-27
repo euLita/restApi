1b490f502ac377e48a3c902e655694bce6163f93
euLita <litas.educ@gmail.com>
2022-11-27 00:33:07 -0400

    Implement edit status attribute

--- pythonProject/first_Api/app.py
+++ pythonProject/first_Api/app.py
@@ -6,7 +6,7 @@ tasks = [
     {
         'id': 13,
         'responsible': 'Gael',
-        'task': 'Develop method GET',
+        'task': 'Develop method GET2',
         'status': 'concluded'
     },
     {
@@ -34,6 +34,20 @@ def _delete(id):
         return True
     return False
 
+def _alterar(id, status_new):
+    # identifying index of item in the list
+    id_procurado = None
+    ind = 0
+    for item in tasks:
+        if item["id"] == id:
+            id_procurado = ind
+        ind = ind + 1
+    # change status item
+    if (id_procurado is not None) and id_procurado <= len(tasks):
+        tasks[id_procurado]['status'] = status_new
+        return True
+    return False
+
 @app.route('/tasks/<int:id>/')
 def get_one(id):
     developer = tasks[id]
@@ -54,6 +68,14 @@ def delete(id):
     if not _delete(id):
         st = 404
     return Response({}, status=st)
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update(id):
+    task = request.get_json()
+    if _alterar(id, task['status']):
+        return jsonify({"mensage": f"Item {id} alterado com sucesso"}), 201
+    return jsonify({"erro": f"Item  {id} não encontrado"}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
