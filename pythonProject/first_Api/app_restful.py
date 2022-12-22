6f8190c4736a2fbc93c915aa8636144ce67214ad
euLita <litas.educ@gmail.com>
2022-12-21 22:36:56 -0400

    Get one and change item

--- pythonProject/first_Api/app_restful.py
+++ pythonProject/first_Api/app_restful.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,19 +17,51 @@ developers = [
         'skills': ['Php', 'Node']
     }
 ]
+with open('data.json', 'r') as f:
+  tasks = json.load(f)
+
+def _alterar(id, status_new):
+    # identifying index of item in the list
+    id_procurado = None
+    ind = 0
+    for item in tasks:
+        if item["id"] == id:
+            id_procurado = ind
+        ind = ind + 1
+    # change status item
+    if (id_procurado is not None) and id_procurado <= len(tasks):
+        tasks[id_procurado]['status'] = status_new
+        return True
+    return False
+def _get_by_id(id):
+    # identifying index of item in the list
+    id_procurado = None
+    ind = 0
+    for item in tasks:
+        if item["id"] == id:
+            id_procurado = ind
+        ind = ind + 1
+    # change status item
+
+    if (id_procurado is not None) and id_procurado <= len(tasks):
+        return tasks[id_procurado]
+    return None
 
 class developer(Resource):
-    def get(self):
-        return {'name':'Ruda'}
-        # return 'Hey dev'
-    def put(self):
-        # pass
-        return 'Hey dev'
-    def delete(self):
-        pass
-        return 'Hey dev'
+    def get(self, id):
+        developer = _get_by_id(id)
+        return developer
 
-api.add_resource(developer, '/dev')
+    def put(self, id):
+        task = json.loads(request.data)
+        if _alterar(id, task['status']):
+            return {"mensage": f"Item {id} alterado com sucesso"}, 201
+        return {"erro": f"Item  {id} não encontrado"}, 404
+
+    def delete(self):
+        return developers
+
+api.add_resource(developer, '/dev/<int:id>/')
 
 if __name__ == '__main__':
     app.run(debug=True)
