31416bbf73a40f7f2b39526dd3174bb731fcb585
euLita <litas.educ@gmail.com>
2022-12-28 15:23:13 -0400

    Retrofitting the API with new data models

--- pythonProject/first_Api/app_restful.py
+++ pythonProject/first_Api/app_restful.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from skills import skills, skill
+import mariadb
 import json
 
 app = Flask(__name__)
@@ -8,6 +9,19 @@ api = Api(app)
 
 with open('data.json', 'r') as f:
   tasks = json.load(f)
+
+data = []
+with open('config.local.json') as f:
+    data = json.load(f)
+
+config = {
+    'host': data['host'],
+    'port': int(data['port']),
+    'user': data['user'],
+    'password': data['password'],
+    'database': data['database'],
+    'autocommit':True
+}
 
 def _alterar(id, status_new):
     # identifying index of item in the list
@@ -38,30 +52,32 @@ def _delete(id):
     return False
 
 def _insert(task_new):
-    if not ('id' in task_new):
-        task_new['id'] = len(tasks)
-    # Validate data
-    for item in tasks:
-        if task_new['id'] == item['id']:
-            return False
-    # Insert data
-    tasks.append(task_new)
-    return True
-
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    sql = f"""INSERT INTO restapi.tasks 
+            (id,owner,status,task) values
+            (null,
+            '{task_new['owner']}',
+            '{task_new['status']}',
+            '{task_new['task']}'
+            );"""
+    print(sql)
+    cur.execute(sql)
+    conn.close()
 
 def _get_by_id(id):
-    # identifying index of item in the list
-    id_procurado = None
-    ind = 0
-    for item in tasks:
-        if item["id"] == id:
-            id_procurado = ind
-        ind = ind + 1
-    # change status item
-
-    if (id_procurado is not None) and id_procurado <= len(tasks):
-        return tasks[id_procurado]
-    return None
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    id_contato = id
+    cur.execute(f"SELECT id, owner, status, task FROM tasks WHERE id = {id_contato};")
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    if not (len(rv) == 0):
+        print(rv[0])
+        tasks = dict(zip(row_headers, rv[0]))
+        return tasks
+    else:
+        return None
 
 class developer(Resource):
     def get(self, id):
